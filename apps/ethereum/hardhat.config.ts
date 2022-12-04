34270daae36b23a35de52d2b0ece3256e347075d
xSNAGSx <99297468+xSNAGSx@users.noreply.github.com>
2022-12-04 19:30:58 +0000

    Fixed Contract test

--- apps/ethereum/hardhat.config.ts
+++ apps/ethereum/hardhat.config.ts
@@ -2,7 +2,19 @@ import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.17",
+  solidity: {
+    compilers: [
+      {
+        version: '0.8.17',
+        settings: {
+          optimizer: {
+            enabled: true,
+            runs: 200,
+          },
+        },
+      },
+    ],
+  },
 };
 
 export default config;

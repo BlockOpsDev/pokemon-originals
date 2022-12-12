c6feafd0a686a433ffda3b4d477233adaebd41a9
Pucarak <pucarak@protonmail.com>
2022-12-12 14:41:21 -0600

    Add Next as Root dep

--- packages/eslint-config-custom/index.js
+++ packages/eslint-config-custom/index.js
@@ -1,5 +1,5 @@
 module.exports = {
-  extends: ["next", "next/babel", "next/core-web-vitals", "turbo", "prettier"],
+  extends: ["next", "turbo", "prettier"],
   rules: {
     "@next/next/no-html-link-for-pages": "off",
     "react/jsx-key": "off",

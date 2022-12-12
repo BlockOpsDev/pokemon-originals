245c80881893a51fdc23211c70b882e26a85bb28
Pucarak <pucarak@protonmail.com>
2022-12-12 12:39:45 -0600

    Fix next/babel error

--- apps/dapp/next.config.js
+++ apps/dapp/next.config.js
@@ -3,6 +3,8 @@ const path = require('path');
 module.exports = {
   reactStrictMode: true,
   experimental: {
-    outputFileTracingRoot: path.join(__dirname, "../../"),
+    turbotrace: {
+      contextDirectory: path.join(__dirname, "../../"),
+    },
 },
 };
--- packages/eslint-config-custom/index.js
+++ packages/eslint-config-custom/index.js
@@ -1,5 +1,5 @@
 module.exports = {
-  extends: ["next", "turbo", "prettier"],
+  extends: ["next", "next/babel", "next/core-web-vitals", "turbo", "prettier"],
   rules: {
     "@next/next/no-html-link-for-pages": "off",
     "react/jsx-key": "off",
